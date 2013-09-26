09b613d00bed390067d396b72f40fcea70bfc498
Pete Bacon Darwin <pete@bacondarwin.com>
2013-09-26 14:37:40 +0100

    rename container element variable

--- extension.js
+++ extension.js
@@ -2,8 +2,7 @@
   angular.module('extension', ['listGroupItems', 'discussionSidebar']);
 
   // Bootstrap the application
-  var pullList = angular.element(document.getElementsByClassName('container'));
-  console.log(pullList);
-  angular.bootstrap(pullList, ['extension']);
+  var container = angular.element(document.getElementsByClassName('container'));
+  angular.bootstrap(container, ['extension']);
 
 })();
