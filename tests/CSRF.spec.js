330f9b58563ddac69d8d2893c3d610a01fa06308
Pete Bacon Darwin <pete@bacondarwin.com>
2013-09-30 13:28:34 +0100

    feat(testing): implement karma testing

---
+++ tests/CSRF.spec.js
@@ -0,0 +1,11 @@
+describe('CSRF', function() {
+  describe('meta directive', function() {
+    beforeEach(module('CSRF'));
+    it('reads the CSRF meta tag and updates the default $http headers', inject(function($compile, $rootScope, $http) {
+      $compile('<head><meta name="csrf-token" content="DUMMY_TOKEN_VALUE"></head>')($rootScope);
+      $rootScope.$digest();
+      expect($http.defaults.headers.put['X-CSRF-Token']).toEqual('DUMMY_TOKEN_VALUE');
+      expect($http.defaults.headers.delete['X-CSRF-Token']).toEqual('DUMMY_TOKEN_VALUE');
+    }));
+  });
+});
