748c1036e0ffc8ae0821b7942335e94120e242b0
Pete Bacon Darwin <pete@bacondarwin.com>
2013-10-09 11:02:01 +0100

    feat(prDetailView): add label filter and hide on blur
    The list of labels in the label-manager is now filtered by the text
    entered in the filter input box.
    
    The label-manager drop-down now hides if you click away from it.
    
    Fixed repeater for the checked label list to "track by" label.name,
    since before it wasn't updating array correctly.  I suspect this is
    due to the filter and orderBy filters reusing array elements.
    
    Closes #6
    Closes #13

--- src/prDetailView.js
+++ src/prDetailView.js
@@ -28,32 +28,48 @@ angular.module('prDetailView', ['labels', 'githubAPI'])
       scope: { labels: '='},
       template: 
         '  <ul class="color-label-list filter-list small">' +
-        '    <li ng-repeat="label in labels | filter : {checked: true}">' +
+        '    <li ng-repeat="label in labels | filter : {checked: true} track by label.name">' +
         '      <gh-label label="label" class="filter-item color-label"></gh-label>' +
         '    </li>' + 
         '  </ul>'
     };
   })
 
-  .directive('labelManager', ['githubAPI', 'orderByFilter', function(githubAPI, orderByFilter) {
+  .directive('labelManager', ['githubAPI', 'orderByFilter', '$timeout', function(githubAPI, orderByFilter, $timeout) {
     return {
       restrict: 'C',
-      link: function(scope) {
-        scope.updateFilteredLabels = function() {
-          scope.filteredLabels = orderByFilter(scope.labels, 'name');
-          scope.labelsLoading = false;
-        };
-        scope.filteredLabels = [];
+      scope: true,
+      link: function(scope, element) {
+        var body = angular.element(document.body);
+
+        function hideModal() {
+          scope.$apply('showModal = false');
+        }
+
         scope.labelsLoading = true;
-        scope.$watch('labels', scope.updateFilteredLabels);
+        scope.$watch('labels', function() { scope.labelsLoading = false; });
         scope.toggleLabel = function(label) {
           label.checked = !label.checked;
           githubAPI.updateLabel(label);
         };
+
+        scope.$watch('showModal', function(showModal) {
+          // We need to do this after the next rendering cycle
+          $timeout(function() {
+            var onOff = showModal ? 'on' : 'off';
+            body[onOff]('click', hideModal);
+          });
+        });
+
+        // Make sure we clean up the click handler on the body if the label handler gets destroyed
+        element.on('$destroy', function() {
+          console.log('destroying');
+          body.off('click', hideModal);
+        });
       },
       template:
       '<strong>Labels</strong>' +
-      '<div class="select-menu label-select-menu" ng-class="{active: showModal}">' +
+      '<div class="select-menu label-select-menu" ng-class="{active: showModal}" ng-click="$event.stopPropagation()">' +
 
         '<span class="minibutton icon-only select-menu-button" ng-click="showModal=!showModal">' +
           '<span class="octicon octicon-gear"></span>' +
@@ -78,7 +94,7 @@ angular.module('prDetailView', ['labels', 'githubAPI'])
             '</div> <!-- /.select-menu-filters -->' +
 
             '<div class="select-menu-list">' +
-              '<div class="select-menu-item" ng-repeat="label in filteredLabels" ng-class="{selected : label.checked}" ng-click="toggleLabel(label)">' +
+              '<div class="select-menu-item" ng-repeat="label in labels | filter : labelFilterField | orderBy : \'name\'" ng-class="{selected : label.checked}" ng-click="toggleLabel(label)">' +
                 '<span class="select-menu-item-icon octicon octicon-check" ></span>' +
                 '<div class="select-menu-item-text">' +
                   '<div class="color-label-wrapper">' +
