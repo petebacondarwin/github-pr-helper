48116866daf637c7078cea249391abb49ece0e32
Neurolit <neurolit@gmail.com>
2014-01-29 23:50:53 +0100

    Remove labels on the detailed view, as Github just added them

--- src/extension.js
+++ src/extension.js
@@ -1,6 +1,6 @@
 (function() {
   // Define the primary extension module
-  angular.module('extension', ['githubHacks', 'flashMessages', 'listGroupItems', 'discussionSidebar']);
+  angular.module('extension', ['githubHacks', 'flashMessages', 'listGroupItems']);
 
   // Bootstrap the application
   angular.bootstrap(document, ['extension']);
